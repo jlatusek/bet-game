b07b6296cab73031d222360efd5291e47b4f8f76
Jakub Latusek <jk.latusek@gmail.com>
2025-05-14 15:32:20 +0200

    Add event implementation

---
+++ include/event.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "io/socket.h"
+
+#include <set>
+#include <sys/epoll.h>
+
+class Epoll;
+
+namespace event
+{
+
+class Event
+{
+  public:
+    explicit Event(io::FileDescriptor::sptr fd, std::set<EPOLL_EVENTS> events);
+    Event() = default;
+    [[nodiscard]] unsigned int get_posix_event() const;
+    [[nodiscard]] io::FileDescriptor::sptr getSocket() const;
+    friend Epoll;
+
+  private:
+    std::shared_ptr<io::FileDescriptor> fd;
+    std::set<EPOLL_EVENTS> listen_events;
+};
+
+} // namespace event
---
+++ src/event.cpp
@@ -0,0 +1,21 @@
+#include "event.h"
+
+event::Event::Event(io::FileDescriptor::sptr fd, std::set<EPOLL_EVENTS> events)
+    : fd(std::move(fd)), listen_events(std::move(events))
+{
+}
+
+unsigned int event::Event::get_posix_event() const
+{
+    unsigned int posix_ev = 0;
+    for (const EPOLL_EVENTS ev : listen_events)
+    {
+        posix_ev |= ev;
+    }
+    return posix_ev;
+}
+
+io::FileDescriptor::sptr event::Event::getSocket() const
+{
+    return fd;
+}

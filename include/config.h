ae46c582f1c241fad83d7b63c3742e389b13d162
Jakub Latusek <jk.latusek@gmail.com>
2025-07-27 00:42:04 +0200

    add variable to define workers

--- include/config.h
+++ include/config.h
@@ -6,6 +6,7 @@ constexpr uint32_t BEGASEP_NUM_MIN = 0xe0ffff00;
 constexpr uint32_t BEGASEP_NUM_MAX = 0xe0ffffaa;
 
 constexpr uint16_t SERVER_PORT = 2222;
+constexpr uint16_t WORKERS = 4;
 
 enum MessageType : uint8_t
 {
